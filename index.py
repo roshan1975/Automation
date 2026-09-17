4fa5d44ceffedcffc181f6ec2a3c1e3f02f63e70
roshan1975 <shahinurislam1975@gmail.com>
2026-09-17 19:33:43 +0600

    Add multiple search engines

--- index.py
+++ index.py
@@ -4,3 +4,11 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
+
+websites = [
+    ("Google", "https://www.google.com", (By.NAME,"q")),
+    ("DuckDuckGo","https://duckduckgo.com",(By.NAME,"q")),
+    ("Bing","https://www.bing.com",(By.NAME,"q")),
+    ("Yahoo","https://search.yahoo.com",(By.NAME,"p")),
+    ("Yandex","https://yandex.com",(By.NAME,"text"))
+]
